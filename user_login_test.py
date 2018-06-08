ce570e5421bb6b2350855193df01db0eba8916e8
grooviq <grooviqdeejay@gmail.com>
2018-06-08 12:20:20 +0300

    add login test init

--- user_login_test.py
+++ user_login_test.py
@@ -12,3 +12,13 @@ class user_test(unittest.TestCase):
         set up method to run each test case
         """
         self.new_user = User("victor maina","grooviqdeejay@gmail.com","grooviq","254groove")
+
+    def test_init(self):
+        """
+        test init test case to test proper object initialization
+        """
+
+        self.assertEqual(self.new_user.fullname,"victor maina")
+        self.assertEqual(self.new_user.email,"grooviqdeejay@gmail.com")
+        self.assertEqual(self.new_user.username, "grooviq")
+        self.assertEqual(self.new_user.password, "254groove")
