8a54a208a219346e89d6a3b554104b084bcd2351
grooviq <grooviqdeejay@gmail.com>
2018-06-08 11:10:44 +0300

    add test init & teardown

--- credential_test.py
+++ credential_test.py
@@ -10,6 +10,19 @@ class credential_test(unittest.TestCase):
     	'''
     	set up method to run each test case
     	'''
-    	self.new_cred = Credential ("facebook","grooviq","grooviqdeejay2gmail.com","password")
+    	self.new_cred = Credential ("facebook","grooviq","grooviqdeejay@gmail.com","254groove")
 
-    
+    def test_init(self):
+    	'''
+    	test init to test the object initialization
+    	'''
+    	self.assertEqual(self.new_cred.accountName, "facebook")
+    	self.assertEqual(self.new_cred.username, "Grooviq")
+    	self.assertEqual(self.new_cred.email, "grooviqdeejay@gmail.com")
+    	self.assertEqual(self.new_cred.password, "254groove")
+
+    def tearDown(self):
+    	'''
+    	tear down metthod that cleans up after running each testcase
+    	'''
+    	Credential.cred_list = []
