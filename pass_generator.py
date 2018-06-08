0630d7eb9699d54fa0b97a94b180e1b9d5b47297
grooviq <grooviqdeejay@gmail.com>
2018-06-08 14:22:40 +0300

    add alternative generator

---
+++ pass_generator.py
@@ -0,0 +1,27 @@
+# import random
+# def random_pass():
+#     rand_num = random.randint(0, 9)
+#     rand_num1 = random.randint(0, 9)
+#     rand_num2 = random.randint(0, 9)
+#     rand_num3 = random.randint(0, 9)
+#     rand_num4 = random.randint(0, 9)
+#     rand_num5 = random.randint(0, 9)
+
+#     print('How many numbers do you want in your password.')
+#     random_pass_choice = input()
+#     if random_pass_choice == '1':
+#         print('your password is ' + rand_num)
+#     if random_pass_choice == '2':
+#         print('your password is ' + str(rand_num) + str(rand_num1))
+#     if random_pass_choice == '3':
+#         print('your password is ' + str(rand_num) + str(rand_num1) + str(rand_num2))
+#     if random_pass_choice == '4':
+#         print('your password is ' + str(rand_num) + str(rand_num1) + str(rand_num2) + str(rand_num3))
+#     if random_pass_choice == '5':
+#         print('your password is ' + str(rand_num) + str(rand_num1) + str(rand_num2) + str(rand_num3) + str(rand_num4))
+
+#     else:
+#         random_pass()
+
+# random_pass()
+        
