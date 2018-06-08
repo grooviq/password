74a5da02da8ba6da10d31021d6a83181f2c16aea
grooviq <grooviqdeejay@gmail.com>
2018-06-08 12:24:23 +0300

    add save user test

--- user_login_test.py
+++ user_login_test.py
@@ -37,3 +37,12 @@ class user_test(unittest.TestCase):
         teardown method that does 'clean up' after each test case has run
         """
         User.user_list = []
+
+    
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+         the user list
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
